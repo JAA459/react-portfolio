96170d3c75a5d07f72c624595724371cac8db89b
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-04-12 11:14:30 -0700

    about page added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Index from './Components/Home/index';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
    <Index />
+   <About />
     </div>
   );
 }
